1200e411637220abc56eb149ddf0f5cf61c094dd
intercept6 <4459220+intercept6@users.noreply.github.com>
2021-06-06 10:03:59 +0900

    ✨ use cdk assets

--- lib/cdk-docker-image-tag-strategy-stack.ts
+++ lib/cdk-docker-image-tag-strategy-stack.ts
@@ -1,7 +1,19 @@
 import {App, Stack, StackProps} from '@aws-cdk/core';
+import {resolve} from 'path';
+
+import {ApplicationLoadBalancedFargateService} from '@aws-cdk/aws-ecs-patterns';
+import {ContainerImage} from '@aws-cdk/aws-ecs';
 
 export class CdkDockerImageTagStrategyStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new ApplicationLoadBalancedFargateService(this, 'service', {
+      publicLoadBalancer: true,
+      taskImageOptions: {
+        image: ContainerImage.fromAsset(resolve('app')),
+        containerPort: 3000,
+      },
+    });
   }
 }
