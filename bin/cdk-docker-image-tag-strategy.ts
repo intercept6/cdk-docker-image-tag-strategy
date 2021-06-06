130368a174f68f7c8712f33d094839ffc66c82aa
intercept6 <4459220+intercept6@users.noreply.github.com>
2021-06-06 12:13:05 +0900

    ✨ use commit hash for image tag

--- bin/cdk-docker-image-tag-strategy.ts
+++ bin/cdk-docker-image-tag-strategy.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import {App} from '@aws-cdk/core';
 import {CdkDockerImageTagStrategyStack} from '../lib/cdk-docker-image-tag-strategy-stack';
+import {execSync} from 'child_process';
+
+const getCommitHashLong = () =>
+  execSync('git rev-parse HEAD')
+    .toString()
+    .replace(/\r?\n|\r/g, '');
 
 const app = new App();
-new CdkDockerImageTagStrategyStack(app, 'CdkDockerImageTagStrategyStack');
+new CdkDockerImageTagStrategyStack(app, 'CdkDockerImageTagStrategyStack', {
+  imageTag: getCommitHashLong(),
+});
--- lib/cdk-docker-image-tag-strategy-stack.ts
+++ lib/cdk-docker-image-tag-strategy-stack.ts
@@ -1,17 +1,45 @@
-import {App, Stack, StackProps} from '@aws-cdk/core';
+import {App, RemovalPolicy, Stack, StackProps} from '@aws-cdk/core';
+import {DockerImageAsset} from '@aws-cdk/aws-ecr-assets';
+import {Repository, TagMutability} from '@aws-cdk/aws-ecr';
+import {ECRDeployment, DockerImageName} from 'cdk-ecr-deployment';
 import {resolve} from 'path';
 
 import {ApplicationLoadBalancedFargateService} from '@aws-cdk/aws-ecs-patterns';
 import {ContainerImage} from '@aws-cdk/aws-ecs';
 
+export interface CdkDockerImageTagStrategyStackProps extends StackProps {
+  imageTag: string;
+}
+
 export class CdkDockerImageTagStrategyStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
+  constructor(
+    scope: App,
+    id: string,
+    props: CdkDockerImageTagStrategyStackProps
+  ) {
     super(scope, id, props);
+
+    const repo = new Repository(this, 'repo', {
+      repositoryName: 'app',
+      imageTagMutability: TagMutability.IMMUTABLE,
+      imageScanOnPush: true,
+      // 検証用のコードなのでゴミを残さないためにDESTROY
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const image = new DockerImageAsset(this, 'image', {
+      directory: resolve('app'),
+    });
+
+    new ECRDeployment(this, 'DeployDockerImage', {
+      src: new DockerImageName(image.imageUri),
+      dest: new DockerImageName(`${repo.repositoryUri}:${props.imageTag}`),
+    });
 
     new ApplicationLoadBalancedFargateService(this, 'service', {
       publicLoadBalancer: true,
       taskImageOptions: {
-        image: ContainerImage.fromAsset(resolve('app')),
+        image: ContainerImage.fromEcrRepository(repo, props.imageTag),
         containerPort: 3000,
       },
     });
